36dc1f6d2f18ca9058aabce257f749acc3d30432
ll0s0ll <llillill@mac.com>
2014-01-22 09:43:11 +0900

    ライセンス条項追加

--- src/mypackage/StationsScreen.java
+++ src/mypackage/StationsScreen.java
@@ -1,3 +1,25 @@
+/*
+	StationsScreen.java
+	
+	Copyright (C) 2014  Shun ITO
+	
+	This file is part of Cojira.
+
+	Cojira is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	Cojira is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+    along with Cojira.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 package mypackage;
 
 import java.io.IOException;
@@ -9,7 +31,6 @@ import java.util.Vector;
 import javax.microedition.io.HttpConnection;
 
 
-import mypackage.MyPopUpMenuScreen.ItemProvider;
 import mypackage.MyScreen.DialogCommandHandler;
 import net.rim.device.api.command.Command;
 import net.rim.device.api.command.CommandHandler;
@@ -365,8 +386,7 @@ public class StationsScreen extends MainScreen
 	        {
 	        	public void execute(ReadOnlyCommandMetadata metadata, Object context)
 	            {
-	        		MyPopUpMenuScreen _screen = new MyPopUpMenuScreen();	        		
-	        		getUiEngine().pushScreen(_screen);	        		
+	        		;       		
 	            }        	
 	        }
         ));
